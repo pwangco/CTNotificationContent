1f545e2b2b254f3a5b7e99f7192821b09a7a4a04
Peter Wilkniss <peter@iMac.local>
2017-09-10 16:53:01 -0700

    refactor a bit

--- CTNotificationContent/ContentBlog/CTContentBlogController.m
+++ CTNotificationContent/ContentBlog/CTContentBlogController.m
@@ -4,7 +4,9 @@
 static NSString * const kActionRead = @"Read";
 static NSString * const kActionKeep = @"Keep";
 
-@interface CTContentBlogController ()
+@interface CTContentBlogController () {
+    NSDictionary *model;
+}
 
 @end
 
@@ -15,16 +17,58 @@ static NSString * const kActionKeep = @"Keep";
 }
 
 - (void)configureViewForContent:(UNNotificationContent *)content {
-    NSLog(@"Content BlogPost: %@", content);
     NSLog(@"Options: %@", self.options);
+    
+    NSDictionary *userInfo = content.userInfo;
+    NSDictionary *data = [userInfo objectForKey:@"d"];
+    if (data && [data isKindOfClass:[NSString class]]) {
+        NSString *_jsonString = (NSString *) data;
+        NSData *_data = [_jsonString dataUsingEncoding:NSUTF8StringEncoding];
+        
+        NSError *error;
+        data = [NSJSONSerialization JSONObjectWithData:_data options:0 error:&error];
+        
+        if (error) {
+            data = nil;
+        }
+    }
+    model = [data objectForKey:@"m"];
+    if (!model || ![model isKindOfClass:[NSDictionary class]]) {
+        model = nil;
+    }
+    
+    NSLog(@"Content Blog model: %@", model);
 }
 
 - (UNNotificationContentExtensionResponseOption)handleAction:(NSString *)action {
     if ([action isEqualToString:kActionRead]) {
-        [[self getParentViewController] openUrl:[NSURL new]];
+        [self launchHostApp];
         return UNNotificationContentExtensionResponseOptionDismiss;
     }
     return UNNotificationContentExtensionResponseOptionDismissAndForwardAction;
+}
+
+
+-(void)launchHostApp {
+    if (!model) {
+        return;
+    }
+    
+    NSString *customScheme;
+    if(self.options) {
+        customScheme = [self.options objectForKey:@"customScheme"];
+    }
+    
+    NSString *cUID = [model objectForKey:@"c_uid"];
+    NSString *objectType = [model objectForKey:@"object_type"];
+    NSString *objectId = [model objectForKey:@"id"];
+    
+    if(!cUID || !objectId || !objectType) return;
+    
+    NSString *path = [NSString stringWithFormat:@"celebs/%@/%@/%@",cUID, objectType, objectId];
+    NSString *urlString = [NSString stringWithFormat:@"%@://%@", customScheme, path];
+    NSURL *url = [NSURL URLWithString:urlString];
+    [[self getParentViewController] openUrl:url];
 }
 
 @end
--- CTNotificationContent/ContentPodcast/CTContentPodcastController.m
+++ CTNotificationContent/ContentPodcast/CTContentPodcastController.m
@@ -3,7 +3,9 @@
 static NSString * const kActionPlay = @"Play";
 static NSString * const kActionKeep = @"Keep";
 
-@interface CTContentPodcastController ()
+@interface CTContentPodcastController () {
+    NSDictionary *model;
+}
 
 @end
 
@@ -14,16 +16,58 @@ static NSString * const kActionKeep = @"Keep";
 }
 
 - (void)configureViewForContent:(UNNotificationContent *)content {
-    NSLog(@"Content Podcast: %@", content);
     NSLog(@"Options: %@", self.options);
+    
+    NSDictionary *userInfo = content.userInfo;
+    NSDictionary *data = [userInfo objectForKey:@"d"];
+    if (data && [data isKindOfClass:[NSString class]]) {
+        NSString *_jsonString = (NSString *) data;
+        NSData *_data = [_jsonString dataUsingEncoding:NSUTF8StringEncoding];
+        
+        NSError *error;
+        data = [NSJSONSerialization JSONObjectWithData:_data options:0 error:&error];
+        
+        if (error) {
+            data = nil;
+        }
+    }
+    model = [data objectForKey:@"m"];
+    if (!model || ![model isKindOfClass:[NSDictionary class]]) {
+        model = nil;
+    }
+    
+     NSLog(@"Content Podcast model: %@", model);
 }
 
 - (UNNotificationContentExtensionResponseOption)handleAction:(NSString *)action {
     if ([action isEqualToString:kActionPlay]) {
-        [[self getParentViewController] openUrl:[NSURL new]];
+        [self launchHostApp];
         return UNNotificationContentExtensionResponseOptionDismiss;
     }
     return UNNotificationContentExtensionResponseOptionDismissAndForwardAction;
+}
+
+
+-(void)launchHostApp {
+    if (!model) {
+        return;
+    }
+    
+    NSString *customScheme;
+    if(self.options) {
+        customScheme = [self.options objectForKey:@"customScheme"];
+    }
+    
+    NSString *cUID = [model objectForKey:@"c_uid"];
+    NSString *objectType = [model objectForKey:@"object_type"];
+    NSString *objectId = [model objectForKey:@"id"];
+    
+    if(!cUID || !objectId || !objectType) return;
+    
+    NSString *path = [NSString stringWithFormat:@"celebs/%@/%@/%@",cUID, objectType, objectId];
+    NSString *urlString = [NSString stringWithFormat:@"%@://%@", customScheme, path];
+    NSURL *url = [NSURL URLWithString:urlString];
+    [[self getParentViewController] openUrl:url];
 }
 
 @end
